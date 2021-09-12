373440b8ce57c77649c45ef86ffdd84438105e74
Thyara <thyaranonato@outlook.com>
2021-09-12 14:33:53 -0300

    Adiciona exercicio 12

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="users">Users</Link></li>
+            <li><Link to="/strict-access"/>Strict Access</li>
           </ul>
         </nav>
         <Switch>
