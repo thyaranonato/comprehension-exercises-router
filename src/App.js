96ccf56493f408f4cac776c6dedfd0f4efb65719
Thyara <thyaranonato@outlook.com>
2021-09-12 11:47:51 -0300

    Adiciona exercicio 5

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import { BrowserRouter, Route } from 'react-router-dom';
 import About from './components/About';
+import Users from './components/Users'
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <BrowserRouter>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
+        <Route exact path="/users" component={Users} />
       </BrowserRouter>
     );
   }
