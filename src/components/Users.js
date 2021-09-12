7253846ed6599d34829294e3ea2672e42a3fdc3c
Thyara <thyaranonato@outlook.com>
2021-09-12 13:54:38 -0300

    Adiciona exercicio 7

--- src/components/Users.js
+++ src/components/Users.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 
 class Users extends Component {
   render() {
+    const { greetingsMessage = 'Hello World' } = this.props
     return (
       <div>
         <h2>Users</h2>
-        <p> My awesome Users component </p>
+        <p> { greetingsMessage }, this is my awesome Users component! </p>
       </div>
     );
   }
